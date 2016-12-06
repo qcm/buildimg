81cff0ff264c5c39665d868760147e272d9eb690
Bo-Yi Lee <boyil@qca.qualcomm.com>
2016-12-05 18:20:23 -0800

    Support RSA exponent up to 65537(0x10001)

--- fdecode.py
+++ fdecode.py
@@ -5,9 +5,9 @@ import binascii
 # ret: returning 0 when successfully write in tlv
 def getRSAData(fname):
 	output = []
-	rsa_str = ''
-	rsa_tmp = ''
-	exponent = '0xff'
+	rsa_bin = ''
+	rsa = ''
+	exponent = '0x00'
 	fin = open(fname, 'r')
 	for line in fin:
 		if 'M' in line:
@@ -15,7 +15,15 @@ def getRSAData(fname):
 		if 'E' in line:
 			ind1 = line.find('(')
 			ind2 = line.find(')')
-			exponent = line[ind1+1:ind2].strip('0xX').zfill(2)
+			exponent = line[ind1+1:ind2].strip('0xX')
+			if len(exponent) == 1:
+				exponent = exponent.zfill(2)	
+			elif len(exponent) == 3:
+				exponent = exponent.zfill(4)	
+			elif len(exponent) == 5:
+				exponent = exponent.zfill(6)	
+			elif len(exponent) == 7:
+				exponent = exponent.zfill(8)	
 		else:
 			tmp1 = line.strip(' :\n') 
 			if len(tmp1) == 0: continue
@@ -24,9 +32,9 @@ def getRSAData(fname):
 	output.pop(0)		
 	output.append(exponent)
 	for i in output:
-		rsa_tmp += i
-		rsa_str += binascii.a2b_hex(i)
-	print rsa_tmp
-	return rsa_str
+		rsa += i
+		rsa_bin += binascii.a2b_hex(i)
+	print rsa
+	return rsa_bin
 
 print getRSAData('rsa.txt')
